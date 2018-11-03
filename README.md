1791f3cf13132c1324123fadb0f3dbfa54f7f1b8
Shoeboxam <shoeboxam@gmail.com>
2018-11-02 21:34:57 -0500

    base sector mapping
    use the most common base sector to reduce the category count

--- README.md
+++ README.md
@@ -2,9 +2,12 @@ Machine Learning Project
 
 # Setup
 1. Download at least one dataset from here:
-https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/28075&version=24.0
+        https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/28075&version=24.0
 
 The latest year is here, probably best to use this one:
 https://dataverse.harvard.edu/file.xhtml?persistentId=doi:10.7910/DVN/28075/POB1OD&version=24.0
 
+The downloaded file must be placed in a new folder called ./data/
+
 2. Run `preprocessing.py`
+
--- preprocessing.py
+++ preprocessing.py
@@ -2,11 +2,13 @@
 import json
 import os
 from datetime import datetime
+from collections import Counter
 
 data_folder = 'data'
 metadata_folder = 'metadata'
 
 
+# parses strings into date objects, strings, floats and ints
 def coerce(key, value):
     datatypes = {
         'Event ID': str,
@@ -42,26 +44,41 @@ def coerce(key, value):
 
 
 def get_data():
-    # linearize the CAMEO lookup
-    reformatter = {}
+    # linearize the class lookups
+    action_reformatter = {}
     with open(os.path.join(os.getcwd(), metadata_folder, 'action.json'), 'r') as actionfile:
         for alignment in json.load(actionfile):
             if 'PLOVER' in alignment and 'CAMEO' in alignment:
-                reformatter[alignment['CAMEO']] = alignment['PLOVER']
+                action_reformatter[alignment['CAMEO']] = alignment['PLOVER']
+
+    sector_reformatter = json.load(open(os.path.join(os.getcwd(), metadata_folder, 'sectors.json'), 'r'))
+
+    def most_common_base_sector(value):
+        if not value:
+            return
+        bases = [sector_reformatter[i] for i in parsed['Source Sectors'].split(',') if i in sector_reformatter]
+        if bases:
+            return Counter(bases).most_common(1)[0][0]
 
     for dataset in os.listdir(os.path.join(os.getcwd(), data_folder)):
         with open(os.path.join(os.getcwd(), data_folder, dataset), 'r', encoding='utf8') as datafile:
+
             headers = next(datafile)[:-1].split('\t')
 
             for line in datafile:
                 parsed = {head: coerce(head, value) for head, value in zip(headers, line[:-1].split('\t'))}
 
-                if parsed['CAMEO Code'] not in reformatter:
+                if parsed['CAMEO Code'] not in action_reformatter:
                     continue
 
-                parsed['PLOVER'] = reformatter[parsed['CAMEO Code']]
+                # construct a new column with the aggregated PLOVER code
+                parsed['PLOVER'] = action_reformatter[parsed['CAMEO Code']]
+
+                parsed['Source Base Sector'] = most_common_base_sector(parsed['Source Sectors'])
+                parsed['Target Base Sector'] = most_common_base_sector(parsed['Target Sectors'])
+
                 yield parsed
 
 
 for observation in get_data():
-    print(observation)
+    pass
